0b3604b7c9914aaf8ecbf612622eb220a623a4b0
Leonardo Nakamichi <leonardo.hideak.nakamichi@gmail.com>
2021-08-04 19:14:53 -0300

    Criado conexão com o banco e tabelas 'autor' e 'usuario'.

--- README.md
+++ README.md
@@ -11,3 +11,17 @@ $ composer install
 ```bash
 $ php -S localhost:8000 -t public
 ```
+
+## Database - criei um novo
+
+https://remotemysql.com/databases.php
+
+Username: R7Owsmotde
+Database name: R7Owsmotde
+Password: K7DVKWmldG
+Server: remotemysql.com
+Port: 3306
+
+These are the username and password to log in to your database and phpMyAdmin
+
+php artisan make:migration class --create=tablename
---
+++ database/migrations/2021_08_04_000818_create_autor_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAutorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('autor', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome_autor', 50)->notNullable();
+            $table->string('nacionalidade_autor', 30)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('autor');
+    }
+}
